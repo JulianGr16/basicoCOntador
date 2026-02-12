350bb1da60b4ade84255506f478a84057e3ce01b
JulianGr16 <juliangaray.2116@gmail.com>
2026-02-12 20:56:11 -0300

    iniciamos un nuevo repositorio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <section className="text-center my-2">
+      <h1>hola mundo</h1>
+      <p className=""> 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae eum
+        aliquid voluptatum reiciendis tenetur expedita maxime dolorem ab, autem
+        maiores quia modi iure amet qui corporis doloremque enim esse?
+      </p>
+    </section>
+  );
+}
+
+export default App;
