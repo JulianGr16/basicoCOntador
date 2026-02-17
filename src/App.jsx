4df2681ef7c3bddce4746b528a8397dff135da58
JulianGr16 <juliangaray.2116@gmail.com>
2026-02-16 21:02:11 -0300

    cree un nuevo componeney lo presente en appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/Informacion";
 function App() {
   return (
-    <section className="text-center my-2">
+    <section className="container text-center my-2">
       <h1>hola mundo</h1>
-      <p className=""> 
+      <p className="px-5"> 
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae eum
         aliquid voluptatum reiciendis tenetur expedita maxime dolorem ab, autem
         maiores quia modi iure amet qui corporis doloremque enim esse?
       </p>
+      <Informacion/>
     </section>
   );
 }
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,15 @@
+const Informacion = () => {
+  return (
+    <section>
+      <h2>¿Ques es react?</h2>
+      <p>
+        React es una biblioteca de JavaScript de código abierto, desarrollada
+        por Facebook, utilizada para construir interfaces de usuario (UI)
+        interactivas y escalables, principalmente para aplicaciones web y
+        móviles
+      </p>
+    </section>
+  );
+};
+
+export default Informacion;
